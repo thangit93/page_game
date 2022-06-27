b19b4166cf18fc3997be461f2a60a5df518e8509
Thang Tran <thangtd1993@gmail.com>
2022-06-27 08:35:02 +0700

    create right menu

--- js/app.js
+++ js/app.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
         $('.choose-character .tab-pane').removeClass('active show')
         $('.choose-character ' + target).addClass('active show');
     })
+
+    $('.btn_hide_menu').click(function () {
+        $('.menu_right').removeClass('animate__slideInRight')
+        $('.menu_right').addClass('animate__slideOutRight')
+        $('.btn_show_menu').show()
+    })
+    $('.btn_show_menu').click(function () {
+        $('.menu_right').removeClass('animate__slideOutRight')
+        $('.menu_right').addClass('animate__slideInRight')
+        $('.btn_show_menu').hide()
+    })
+
+    $("#btn_go_top").click(function() {
+        $("html, body").animate({scrollTop: 0}, 1000);
+    })
 })
